9e2bd86ddcf4f805d277b085df585f674fca83ed
Jörg Kullrich <joerg.kullrich@siemens.com>
2025-02-24 13:55:41 +0100

    enhance CLI: add support for custom registry URL and authentication token

--- ipm/pkg/cli/cli.go
+++ ipm/pkg/cli/cli.go
@@ -10,47 +10,61 @@ import (
 )
 
 func NewRootCmd() *cobra.Command {
-    var logLevel string
-    var logFile string
-    var jsonOutput bool
+	var logLevel string
+	var logFile string
+	var jsonOutput bool
+	var registryURL string
+	var registryToken string
 
-    rootCmd := &cobra.Command{
-        Use:   "ipm",
-        Short: "Industrial Package Manager",
-        Long:  "A secure, extensible package manager for industrial applications.",
-        PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-            return log.Init(logLevel, logFile)
-        },
-    }
+	rootCmd := &cobra.Command{
+		Use:   "ipm",
+		Short: "Industrial Package Manager",
+		Long:  "A secure, extensible package manager for industrial applications.",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			// Zuerst Logger initialisieren
+			if err := log.Init(logLevel, logFile); err != nil {
+				return err
+			}
+			// Danach Debug-Log schreiben
+			log.Debug("Initializing with registry configuration", map[string]interface{}{
+				"registryURL":  registryURL,
+				"registryToken": registryToken,
+			})
+			return nil
+		},
+	}
 
-    reg := registry.NewNPMRegistry()
-    inst := installer.NewInstaller()
+	rootCmd.PersistentFlags().StringVar(&registryURL, "registry", "https://registry.npmjs.org", "Custom registry URL (e.g., https://npm.pkg.github.com)")
+	rootCmd.PersistentFlags().StringVar(&registryToken, "token", "", "Authentication token for the registry")
+	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "", "Set log level (debug, info, error) to enable logging")
+	rootCmd.PersistentFlags().StringVar(&logFile, "log-file", "", "Write logs to specified file")
+	rootCmd.PersistentFlags().BoolVar(&jsonOutput, "json", false, "Output in JSON format")
 
-    installCmd := &cobra.Command{
-        Use:   "install [package[@version]]",
-        Short: "Install a package",
-        Args:  cobra.MinimumNArgs(1),
-        RunE: func(cmd *cobra.Command, args []string) error {
-            return inst.Install(reg, args[0], jsonOutput)
-        },
-    }
+	// Registry und Installer nach Flag-Definition initialisieren
+	reg := registry.NewNPMRegistry(registryURL, registryToken)
+	inst := installer.NewInstaller(reg)
 
-    rootCmd.AddCommand(installCmd)
-    rootCmd.AddCommand(&cobra.Command{
-        Use:   "version",
-        Short: "Print the version",
-        Run: func(cmd *cobra.Command, args []string) {
-            if jsonOutput {
-                fmt.Println(`{"version": "0.1.0"}`)
-            } else {
-                fmt.Println("IPM v0.1.0")
-            }
-        },
-    })
+	installCmd := &cobra.Command{
+		Use:   "install [package[@version]]",
+		Short: "Install a package",
+		Args:  cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return inst.Install(reg, args[0], jsonOutput)
+		},
+	}
 
-    rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "", "Set log level (debug, info, error) to enable logging")
-    rootCmd.PersistentFlags().StringVar(&logFile, "log-file", "", "Write logs to specified file")
-    rootCmd.PersistentFlags().BoolVar(&jsonOutput, "json", false, "Output in JSON format")
+	rootCmd.AddCommand(installCmd)
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "Print the version",
+		Run: func(cmd *cobra.Command, args []string) {
+			if jsonOutput {
+				fmt.Println(`{"version": "0.1.0"}`)
+			} else {
+				fmt.Println("IPM v0.1.0")
+			}
+		},
+	})
 
-    return rootCmd
+	return rootCmd
 }
--- ipm/pkg/registry/registry.go
+++ ipm/pkg/registry/registry.go
@@ -12,153 +12,183 @@ import (
 )
 
 type Registry interface {
-    FetchPackageTarball(name, version string) (io.ReadCloser, types.Package, error)
-    ResolveVersion(name, versionRange string) (string, error)
+	FetchPackageTarball(name, version string) (io.ReadCloser, types.Package, error)
+	ResolveVersion(name, versionRange string) (string, error)
 }
 
 type NPMRegistry struct {
-    BaseURL string
+	BaseURL string
+	Token   string
+	Client  *http.Client
 }
 
-func NewNPMRegistry() *NPMRegistry {
-    return &NPMRegistry{BaseURL: "https://registry.npmjs.org"}
+func NewNPMRegistry(baseURL, token string) *NPMRegistry {
+	return &NPMRegistry{
+		BaseURL: baseURL,
+		Token:   token,
+		Client:  &http.Client{},
+	}
 }
 
 func (r *NPMRegistry) FetchPackageTarball(name, version string) (io.ReadCloser, types.Package, error) {
-    metadataURL := fmt.Sprintf("%s/%s/%s", r.BaseURL, name, version)
-    log.Debug("Sending request to npm registry", map[string]interface{}{
-        "url": metadataURL,
-    })
+	metadataURL := fmt.Sprintf("%s/%s/%s", r.BaseURL, name, version)
+	log.Debug("Sending request to registry", map[string]interface{}{
+		"url": metadataURL,
+	})
 
-    resp, err := http.Get(metadataURL)
-    if err != nil {
-        log.Error("Failed to fetch metadata", err, map[string]interface{}{
-            "package": name,
-            "version": version,
-        })
-        return nil, types.Package{}, fmt.Errorf("failed to fetch metadata for %s@%s: %v", name, version, err)
-    }
-    defer resp.Body.Close()
+	req, err := http.NewRequest("GET", metadataURL, nil)
+	if err != nil {
+		return nil, types.Package{}, fmt.Errorf("failed to create request: %v", err)
+	}
+	if r.Token != "" {
+		req.Header.Set("Authorization", "Bearer "+r.Token)
+	}
 
-    if resp.StatusCode != http.StatusOK {
-        log.Error("Metadata request failed", nil, map[string]interface{}{
-            "status": resp.Status,
-            "url":    metadataURL,
-        })
-        return nil, types.Package{}, fmt.Errorf("metadata request failed with status: %s", resp.Status)
-    }
+	resp, err := r.Client.Do(req)
+	if err != nil {
+		log.Error("Failed to fetch metadata", err, map[string]interface{}{
+			"package": name,
+			"version": version,
+		})
+		return nil, types.Package{}, fmt.Errorf("failed to fetch metadata for %s@%s: %v", name, version, err)
+	}
+	defer resp.Body.Close()
 
-    var pkgData struct {
-        Name         string            `json:"name"`
-        Version      string            `json:"version"`
-        Dist         struct {
-            Tarball string `json:"tarball"`
-        } `json:"dist"`
-        Dependencies map[string]string `json:"dependencies"`
-    }
-    if err := json.NewDecoder(resp.Body).Decode(&pkgData); err != nil {
-        log.Error("Failed to parse metadata", err, map[string]interface{}{
-            "package": name,
-            "version": version,
-        })
-        return nil, types.Package{}, fmt.Errorf("failed to parse metadata: %v", err)
-    }
+	if resp.StatusCode != http.StatusOK {
+		log.Error("Metadata request failed", nil, map[string]interface{}{
+			"status": resp.Status,
+			"url":    metadataURL,
+		})
+		return nil, types.Package{}, fmt.Errorf("metadata request failed with status: %s", resp.Status)
+	}
 
-    log.Debug("Package metadata fetched", map[string]interface{}{
-        "package": pkgData.Name,
-        "version": pkgData.Version,
-    })
+	var pkgData struct {
+		Name         string            `json:"name"`
+		Version      string            `json:"version"`
+		Dist         struct {
+			Tarball string `json:"tarball"`
+		} `json:"dist"`
+		Dependencies map[string]string `json:"dependencies"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&pkgData); err != nil {
+		log.Error("Failed to parse metadata", err, map[string]interface{}{
+			"package": name,
+			"version": version,
+		})
+		return nil, types.Package{}, fmt.Errorf("failed to parse metadata: %v", err)
+	}
 
-    tarballResp, err := http.Get(pkgData.Dist.Tarball)
-    if err != nil {
-        log.Error("Failed to fetch tarball", err, map[string]interface{}{
-            "url": pkgData.Dist.Tarball,
-        })
-        return nil, types.Package{}, fmt.Errorf("failed to fetch tarball for %s@%s: %v", name, version, err)
-    }
-    if tarballResp.StatusCode != http.StatusOK {
-        tarballResp.Body.Close()
-        log.Error("Tarball request failed", nil, map[string]interface{}{
-            "status": tarballResp.Status,
-            "url":    pkgData.Dist.Tarball,
-        })
-        return nil, types.Package{}, fmt.Errorf("tarball request failed with status: %s", tarballResp.Status)
-    }
+	log.Debug("Package metadata fetched", map[string]interface{}{
+		"package": pkgData.Name,
+		"version": pkgData.Version,
+	})
 
-    pkg := types.Package{
-        Name:    pkgData.Name,
-        Version: pkgData.Version,
-        Deps:    pkgData.Dependencies,
-    }
-    return tarballResp.Body, pkg, nil
+	tarballReq, err := http.NewRequest("GET", pkgData.Dist.Tarball, nil)
+	if err != nil {
+		return nil, types.Package{}, fmt.Errorf("failed to create tarball request: %v", err)
+	}
+	if r.Token != "" {
+		tarballReq.Header.Set("Authorization", "Bearer "+r.Token)
+	}
+
+	tarballResp, err := r.Client.Do(tarballReq)
+	if err != nil {
+		log.Error("Failed to fetch tarball", err, map[string]interface{}{
+			"url": pkgData.Dist.Tarball,
+		})
+		return nil, types.Package{}, fmt.Errorf("failed to fetch tarball for %s@%s: %v", name, version, err)
+	}
+	if tarballResp.StatusCode != http.StatusOK {
+		tarballResp.Body.Close()
+		log.Error("Tarball request failed", nil, map[string]interface{}{
+			"status": tarballResp.Status,
+			"url":    pkgData.Dist.Tarball,
+		})
+		return nil, types.Package{}, fmt.Errorf("tarball request failed with status: %s", tarballResp.Status)
+	}
+
+	pkg := types.Package{
+		Name:    pkgData.Name,
+		Version: pkgData.Version,
+		Deps:    pkgData.Dependencies,
+	}
+	return tarballResp.Body, pkg, nil
 }
 
 func (r *NPMRegistry) ResolveVersion(name, versionRange string) (string, error) {
-    metadataURL := fmt.Sprintf("%s/%s", r.BaseURL, name)
-    log.Debug("Sending request to npm registry for version resolution", map[string]interface{}{
-        "url": metadataURL,
-    })
+	metadataURL := fmt.Sprintf("%s/%s", r.BaseURL, name)
+	log.Debug("Sending request to registry for version resolution", map[string]interface{}{
+		"url": metadataURL,
+	})
 
-    resp, err := http.Get(metadataURL)
-    if err != nil {
-        log.Error("Failed to fetch metadata for version resolution", err, map[string]interface{}{
-            "package": name,
-        })
-        return "", fmt.Errorf("failed to fetch metadata for %s: %v", name, err)
-    }
-    defer resp.Body.Close()
+	req, err := http.NewRequest("GET", metadataURL, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create request: %v", err)
+	}
+	if r.Token != "" {
+		req.Header.Set("Authorization", "Bearer "+r.Token)
+	}
 
-    if resp.StatusCode != http.StatusOK {
-        log.Error("Metadata request failed", nil, map[string]interface{}{
-            "status": resp.Status,
-            "url":    metadataURL,
-        })
-        return "", fmt.Errorf("metadata request failed with status: %s", resp.Status)
-    }
+	resp, err := r.Client.Do(req)
+	if err != nil {
+		log.Error("Failed to fetch metadata for version resolution", err, map[string]interface{}{
+			"package": name,
+		})
+		return "", fmt.Errorf("failed to fetch metadata for %s: %v", name, err)
+	}
+	defer resp.Body.Close()
 
-    var pkgData struct {
-        Versions map[string]interface{} `json:"versions"`
-    }
-    if err := json.NewDecoder(resp.Body).Decode(&pkgData); err != nil {
-        log.Error("Failed to parse metadata", err, map[string]interface{}{
-            "package": name,
-        })
-        return "", fmt.Errorf("failed to parse metadata: %v", err)
-    }
+	if resp.StatusCode != http.StatusOK {
+		log.Error("Metadata request failed", nil, map[string]interface{}{
+			"status": resp.Status,
+			"url":    metadataURL,
+		})
+		return "", fmt.Errorf("metadata request failed with status: %s", resp.Status)
+	}
 
-    constraint, err := semver.NewConstraint(versionRange)
-    if err != nil {
-        log.Error("Invalid version range", err, map[string]interface{}{
-            "range": versionRange,
-        })
-        return "", fmt.Errorf("invalid version range %s: %v", versionRange, err)
-    }
+	var pkgData struct {
+		Versions map[string]interface{} `json:"versions"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&pkgData); err != nil {
+		log.Error("Failed to parse metadata", err, map[string]interface{}{
+			"package": name,
+		})
+		return "", fmt.Errorf("failed to parse metadata: %v", err)
+	}
 
-    var latest *semver.Version
-    for verStr := range pkgData.Versions {
-        ver, err := semver.NewVersion(verStr)
-        if err != nil {
-            continue
-        }
-        if constraint.Check(ver) {
-            if latest == nil || ver.GreaterThan(latest) {
-                latest = ver
-            }
-        }
-    }
+	constraint, err := semver.NewConstraint(versionRange)
+	if err != nil {
+		log.Error("Invalid version range", err, map[string]interface{}{
+			"range": versionRange,
+		})
+		return "", fmt.Errorf("invalid version range %s: %v", versionRange, err)
+	}
 
-    if latest == nil {
-        log.Error("No version found matching range", nil, map[string]interface{}{
-            "package": name,
-            "range":   versionRange,
-        })
-        return "", fmt.Errorf("no version found for %s matching %s", name, versionRange)
-    }
+	var latest *semver.Version
+	for verStr := range pkgData.Versions {
+		ver, err := semver.NewVersion(verStr)
+		if err != nil {
+			continue
+		}
+		if constraint.Check(ver) {
+			if latest == nil || ver.GreaterThan(latest) {
+				latest = ver
+			}
+		}
+	}
 
-    log.Debug("Version resolved", map[string]interface{}{
-        "package": name,
-        "range":   versionRange,
-        "version": latest.Original(),
-    })
-    return latest.Original(), nil
+	if latest == nil {
+		log.Error("No version found matching range", nil, map[string]interface{}{
+			"package": name,
+			"range":   versionRange,
+		})
+		return "", fmt.Errorf("no version found for %s matching %s", name, versionRange)
+	}
+
+	log.Debug("Version resolved", map[string]interface{}{
+		"package": name,
+		"range":   versionRange,
+		"version": latest.Original(),
+	})
+	return latest.Original(), nil
 }
